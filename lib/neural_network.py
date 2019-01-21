be30e5254e712d9128550a63a8de8bec22798353
Rohan Almeida <rohan.almeida@ril.com>
2019-01-21 20:00:56 +0530

    initial commit

---
+++ lib/neural_network.py
@@ -0,0 +1,14 @@
+
+class neuralNetwork:
+    def __init__(self, inodes, hnodes, onodes, learning_rate):
+        self.inodes = inodes
+        self.hnodes = hnodes
+        self.onodes = onodes
+        self.lr = learning_rate
+
+    def train():
+        pass
+
+    def query():
+        pass
+    
